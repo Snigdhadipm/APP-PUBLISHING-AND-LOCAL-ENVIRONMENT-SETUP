24fbc157287f094bb1f7925ef2ffb0532701f855
Snigdhadipm <85871995+Snigdhadipm@users.noreply.github.com>
2022-06-13 00:40:23 +0530

    Add files via upload

---
+++ schoolattendanceapp--template/config.js
@@ -0,0 +1,19 @@
+ import * as firebase from "firebase";
+
+//initialize your database
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXfQN8snzBmSp7DXUXgjFzqDP1UuNRNJU",
+  authDomain: "school-attendance-app-374a3.firebaseapp.com",
+  databaseURL: "https://school-attendance-app-374a3-default-rtdb.firebaseio.com",
+  projectId: "school-attendance-app-374a3",
+  storageBucket: "school-attendance-app-374a3.appspot.com",
+  messagingSenderId: "852392785199",
+  appId: "1:852392785199:web:ba504f55f8533981971dda",
+  measurementId: "${config.measurementId}"
+};
+
+  export default firebase.database()
+ 
+
+  
